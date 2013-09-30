42730bdea567ead5ce4d75f887ec4c27500cf3bb
rushcut <rushcut@gmail.com>
2013-09-30 22:47:45 +0900

    shell use zsh

--- init-exec-path.el
+++ init-exec-path.el
@@ -4,8 +4,9 @@
   (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
-
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
+
+(setenv "ESHELL" (expand-file-name "/bin/zsh"))
 
 (provide 'init-exec-path)
