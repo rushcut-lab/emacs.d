b53d63ed4826fbff9e6a7f6b2ac3abee5dfde520
rushcut <rushcut@gmail.com>
2013-10-01 09:00:04 +0900

    disabled ` smartparens in feature-mode.

--- init-parens.el
+++ init-parens.el
@@ -1,5 +1,8 @@
 (require-package 'smartparens)
-(smartparens-global-mode 1)
 (require 'smartparens-config)
+(smartparens-global-mode t)
+
+(sp-with-modes '(feature-mode)
+  (sp-local-pair "'" nil :actions nil))
 
 (provide 'init-parens)
